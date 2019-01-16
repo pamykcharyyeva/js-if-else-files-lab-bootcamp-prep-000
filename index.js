9d332feb4517ec0605cf742d795e433e80095088
Pamyk Charyyeva <pamyk.ch@gmail.com>
2019-01-16 04:24:53 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 var name = "Joe";
 var height = "74"
 if (name === "Susan") {
-  console.log("The name is correct");
+  console.log("The name is correct")
+} else {
+  console.log ("Expect the name to be Susan")
 }
 
 
